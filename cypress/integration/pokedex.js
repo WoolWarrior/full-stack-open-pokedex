16e217ba9f60b47f2e84d7c9b3ce09d3a88d0c94
Wool Warrior <awoolwarrior@gmail.com>
2021-05-24 12:35:58 +0100

    update test, update github action

--- cypress/integration/pokedex.js
+++ cypress/integration/pokedex.js
@@ -4,4 +4,14 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  describe('go to a pokemon page', function() {
+    beforeEach(function() {
+      cy.visit('http://localhost:5000')
+    })
+    it('go to Ivysaur', function() {
+      cy.get('a[href*="ivysaur"]').click()
+      cy.contains('chlorophyll')
+    })
+  })
 })
